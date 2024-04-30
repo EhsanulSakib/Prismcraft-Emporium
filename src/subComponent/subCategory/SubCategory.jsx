24eec89d38140c454352840d9a80f958bf579c97
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2024-04-30 22:20:11 +0600

    subCategory in homepage added

--- src/subComponent/subCategory/SubCategory.jsx
+++ src/subComponent/subCategory/SubCategory.jsx
@@ -1,14 +1,27 @@
-
+import { Typewriter } from 'react-simple-typewriter'
 const SubCategory = ({ subCategory_info }) => {
     const { Category, image, subCategory } = subCategory_info
     return (
         <div className="card shadow-xl image-full">
             <figure><img src={image} alt="" /></figure>
             <div className="card-body">
-                <h2 className="card-title font-extrabold text-xl">{Category}</h2>
-                <p></p>
+                <h2 className="card-title font-extrabold text-xl text-white">{Category}</h2>
+                <h1 className='pt-0' style={{ paddingTop: '0.5rem', margin: 'auto 0', fontWeight: 'normal' }}>
+                    {' '}
+                    <span style={{ color: 'white', fontWeight: 'bold' }}>
+                        {/* Style will be inherited from the parent element */}
+                        <Typewriter
+                            words={subCategory}
+                            loop={false}
+                            cursor
+                            cursorStyle='_'
+                            typeSpeed={30}
+                            deleteSpeed={20}
+                            delaySpeed={1000}
+                        />
+                    </span>
+                </h1>
                 <div className="card-actions justify-end">
-                    <button className="btn">Buy Now</button>
                 </div>
             </div>
         </div>
